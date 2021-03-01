072f4ccfd3f291c6168d551ee1f93e6d76def192
Kevin Helgsten <xatron3@gmail.com>
2021-03-01 02:41:11 +0100

    Test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// vue.config.js file to be place in the root of your repository
+// vue.config.js file to be place in the root of your repositor
 
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/cheapdonut/" : "/",
